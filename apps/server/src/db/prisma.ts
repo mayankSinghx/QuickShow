a35de93f9d44c8a651c493590a39cb4b7f4faa1e
mayankxcrino <mayank@xcrino.com>
2026-01-31 09:14:55 +0530

    chnges

--- apps/server/src/db/prisma.ts
+++ apps/server/src/db/prisma.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const isLocalhost = process.env.DATABASE_URL?.includes('localhost') || process.env.DATABASE_URL?.includes('127.0.0.1');
 
